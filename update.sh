4d2f25dcfe08f630f88063d863584999db275300
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2020-01-12 20:42:13 +0100

    Fix update script to handle deleted files

--- update.sh
+++ update.sh
@@ -7,9 +7,20 @@ WWW_PATH="www"
 
 echo "get commit.hash" | sftp codex
 echo "cd ${WWW_PATH}" > batch_update
-for f in $(git diff `cat commit.hash` HEAD --name-only); do echo "put $f `dirname $f`" >> batch_update; done
+echo "cd ${WWW_PATH}" > batch_delete
+git diff `cat commit.hash` HEAD --name-status | while read line; do
+    stat_path=($line)
+    mod=${stat_path[0]}
+    if [ "${stat_path[0]}" == "D" ]; then
+        echo "rm ${stat_path[1]}" >> batch_delete;
+    else
+        echo "put ${stat_path[1]} `dirname ${stat_path[1]}`" >> batch_update;
+    fi
+done
 sftp -b batch_update codex
+sftp -b batch_delete codex
 rm batch_update
+rm batch_delete
 git rev-parse HEAD > commit.hash
 echo "put commit.hash" | sftp codex
 rm commit.hash
