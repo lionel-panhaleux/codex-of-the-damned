a549a0b50b2bbf35b3ac0a1f533a34dbfa9775d5
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2020-01-24 19:25:08 +0100

    Cleaner twda.js, new KRCG API

--- twda/twda.js
+++ twda/twda.js
@@ -1,22 +1,54 @@
-async function getDeck(data) {
-    let response = await fetch(
-        `https://krcg.herokuapp.com/deck`, {
-        method: "post",
-        body: JSON.stringify(data),
+function getDeckWithCards(cards) {
+    fetch(
+        // `https://krcg.herokuapp.com/deck`, {
+        `http://127.0.0.1:8000/deck`, {
+        method: "POST",
+        body: JSON.stringify({ "cards": cards }),
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-    }
-    )
-    let result = await response.json()
-    return result
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response;
+        })
+        .then(response => response.json())
+        .then(data => displayDeckChoices(data))
+        .catch(function (error) {
+            document.getElementById("results").textContent = error.message
+        })
 }
-function displayError(message) {
-    document.getElementById("results").textContent = message
+function getDeckByID(element, twda_id) {
+    fetch(
+        // encodeURI(`https://krcg.herokuapp.com/deck/${twda_id}`), {
+        encodeURI(`http://127.0.0.1:8000/deck/${twda_id}`), {
+        method: "GET",
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response;
+        })
+        .then(response => response.json())
+        .then(data => displayDeck(data))
+        .catch(function (error) {
+            document.getElementById("results").textContent = error.message
+        })
+    if (element) {
+        window.history.pushState({ "twda_id": twda_id }, "TWDA", `?twda_id=${twda_id}`)
+        for (let sibling of element.parentElement.children) {
+            sibling.classList.remove("selected")
+        }
+        element.classList.add("selected")
+    }
 }
 function cardElement(element) {
-    let reference = element[1].toLowerCase()
+    let reference = element["name"].toLowerCase()
     if (reference.startsWith("the ")) {
         reference = reference.substr(4, reference.length) + "the"
     }
@@ -25,8 +57,8 @@ function cardElement(element) {
     reference = reference.replace("ö", "o") // Rötschreck
     reference = reference.replace("ç", "c") // Monçada
     reference = reference.replace("ó", "o") // Dónal
-    const name = element[1].replace("(TM) ", "™ ")
-    return `<li>${element[0]} <span class="card" onclick="dC('${reference}')">${name}</span></li>\n`
+    const name = element["name"].replace("(TM) ", "™ ")
+    return `<li>${element["count"]} <span class="card" onclick="dC('${reference}')">${name}</span></li>\n`
 }
 function wrapText(text, maxlen) {
     if (!text) {
@@ -40,51 +72,25 @@ function wrapText(text, maxlen) {
 function removeComments() {
     comments.innerHTML = ""
 }
-function displayDeck(data, twda_id) {
+function displayDeck(data) {
     removeComments()
     document.getElementById("deck-link").textContent = wrapText(data["name"], 25)
-    document.getElementById("deck-link").href = `http://www.vekn.fr/decks/twd.htm#${twda_id}`
+    document.getElementById("deck-link").href = `http://www.vekn.fr/decks/twd.htm#${data["twda_id"]}`
     document.getElementById("deck-header").innerHTML = [
-        data["player"],
+        wrapText(data["player"], 40),
         wrapText(data["event"], 40),
         wrapText(data["place"], 40),
         data["date"],
         data["players_count"] + " players",
     ].join("<br/>")
-    document.getElementById("crypt-header").textContent = `Crypt (${data["crypt"][0]})`
-    data["crypt"][1].map(cardElement).join("\n")
-    document.getElementById("crypt-list").innerHTML = data["crypt"][1].map(cardElement).join("\n")
-    document.getElementById("library-header").textContent = `Library (${data["library"][0]})`
+    document.getElementById("crypt-header").textContent = `Crypt (${data["crypt"]["count"]})`
+    data["crypt"]["cards"].map(cardElement).join("\n")
+    document.getElementById("crypt-list").innerHTML = data["crypt"]["cards"].map(cardElement).join("\n")
+    document.getElementById("library-header").textContent = `Library (${data["library"]["count"]})`
     card_list = ""
-    const TYPE_ORDER = [
-        "Master",
-        "Conviction",
-        "Action",
-        "Action/Combat",
-        "Action/Reaction",
-        "Political Action",
-        "Action Modifier",
-        "Action Modifier/Reaction",
-        "Action Modifier/Combat",
-        "Combat",
-        "Combat/Action",
-        "Combat/Action Modifier",
-        "Combat/Reaction",
-        "Reaction",
-        "Reaction/Action Modifier",
-        "Reaction/Combat",
-        "Power",
-        "Equipment",
-        "Ally",
-        "Retainer",
-        "Event",
-    ]
-    types = Object.entries(data["library"][1]).sort(
-        (a, b) => TYPE_ORDER.indexOf(a[0]) - TYPE_ORDER.indexOf(b[0])
-    )
-    for (let [type, cards] of types) {
-        card_list += `<li><h4>— ${type} (${cards[0]}) —</h4></li>`
-        for (const element of cards[1]) {
+    for (const section of data["library"]["cards"]) {
+        card_list += `<li><h4>— ${section["type"]} (${section["count"]}) —</h4></li>`
+        for (const element of section["cards"]) {
             card_list += cardElement(element)
         }
     }
@@ -98,44 +104,53 @@ function displayDeck(data, twda_id) {
     document.getElementById("decklist").style.display = "block"
     document.getElementById("comments-title").style.display = "block"
 }
-function getAndDisplayDeck(element, twda_id) {
-    getDeck({ "twda_id": twda_id })
-        .then(data => displayDeck(data[0][1], data[0][0]))
-        .catch(reason => displayError(reason.message))
-    if (element) {
-        window.history.pushState({ "twda_id": 1 }, "TWDA", `?twda_id=${twda_id}`)
-        for (let sibling of element.parentElement.children) {
-            sibling.classList.remove("selected")
-        }
-        element.classList.add("selected")
-    }
-}
 function displayDeckChoices(data) {
+    console.log(data)
     removeComments()
     let list = ""
     if (!data || data.length < 1) {
         list = "No result in TWDA."
     }
-    for (const [twda_id, deck] of data) {
+    for (const deck of data) {
         list += `
         <tr class="results">
-            <td class="results date" onclick="getAndDisplayDeck(this.parentNode, '${twda_id}')">${deck["date"]}</td>
-            <td class="results player" onclick="getAndDisplayDeck(this.parentNode, '${twda_id}')">${deck["player"]}</td>
-            <td class="results name" onclick="getAndDisplayDeck(this.parentNode, '${twda_id}')">${deck["name"]}</td>
+            <td class="results date" onclick="getDeckByID(this.parentNode, '${deck["twda_id"]}')">${deck["date"]}</td>
+            <td class="results player" onclick="getDeckByID(this.parentNode, '${deck["twda_id"]}')">${wrapText(deck["player"], 40)}</td>
+            <td class="results name" onclick="getDeckByID(this.parentNode, '${deck["twda_id"]}')">${wrapText(deck["name"], 80)}</td>
         </tr>
         `
     }
     document.getElementById("results").innerHTML = list
 }
-async function fetchCompletion(text) {
-    let response = await fetch(
+function displayCompletion(input, items_list, data) {
+    for (const candidate of data) {
+        let b = document.createElement("DIV")
+        b.textContent = candidate
+        b.addEventListener("click", function (e) {
+            input.value = this.textContent
+            closeAllLists(input)
+            getDeckWithCards([input.value])
+        })
+        items_list.appendChild(b);
+    }
+}
+function fetchCompletion(input, items_list, text) {
+    fetch(
         encodeURI(`https://krcg.herokuapp.com/complete/${text}`), {
         method: "GET",
         headers: { 'Accept': 'application/json' }
-    }
-    )
-    let result = await response.json()
-    return result
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response;
+        })
+        .then(response => response.json())
+        .then(data => displayCompletion(input, items_list, data))
+        .catch(function (error) {
+            document.getElementById("results").textContent = error.message
+        })
 }
 function clearSearch() {
     document.getElementById("decklist").style.display = "none"
@@ -143,40 +158,24 @@ function clearSearch() {
     document.getElementById("results").innerHTML = ""
     removeComments()
 }
-function autocomplete(element) {
+function autocomplete(input) {
     var currentFocus
     function doComplete(e) {
         const val = this.value
-        closeAllLists();
+        closeAllLists(input);
         if (!val || val.length < 3) {
             clearSearch()
             return false
         }
         currentFocus = -1
-        let a = document.createElement("DIV")
-        a.setAttribute("id", this.id + "autocomplete-list")
-        a.setAttribute("class", "autocomplete-items")
-        this.parentNode.appendChild(a)
-        fetchCompletion(val)
-            .then(function (result) {
-                console.log(result)
-                for (const candidate of result) {
-                    let b = document.createElement("DIV")
-                    b.textContent = candidate
-                    b.addEventListener("click", function (e) {
-                        element.value = this.textContent
-                        closeAllLists()
-                        getDeck({ "cards": [element.form.card_name.value] })
-                            .then(data => displayDeckChoices(data))
-                            .catch(reason => displayError(reason.message))
-                    })
-                    a.appendChild(b);
-                }
-            })
-            .catch(reason => displayError(reason.message))
+        let items_list = document.createElement("DIV")
+        items_list.setAttribute("id", this.id + "autocomplete-list")
+        items_list.setAttribute("class", "autocomplete-items")
+        this.parentNode.appendChild(items_list)
+        fetchCompletion(input, items_list, val)
     }
-    element.addEventListener("input", doComplete)
-    element.addEventListener("keydown", function (e) {
+    input.addEventListener("input", doComplete)
+    input.addEventListener("keydown", function (e) {
         let x = document.getElementById(this.id + "autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
         if (e.keyCode === 40) { // arrow DOWN
@@ -188,9 +187,7 @@ function autocomplete(element) {
         } else if (e.keyCode === 13) { // ENTER
             e.preventDefault();
             if (currentFocus > -1 && x) { x[currentFocus].click() }
-            getDeck({ "cards": [element.form.card_name.value] })
-                .then(data => displayDeckChoices(data))
-                .catch(reason => displayError(reason.message))
+            getDeckWithCards([input.value])
         } else if (e.keyCode === 8 || e.keyCode === 46) { // DELETE or BACKSPACE
             doComplete()
         }
@@ -202,19 +199,19 @@ function autocomplete(element) {
         if (currentFocus < 0) { currentFocus = (x.length - 1) }
         x[currentFocus].classList.add("autocomplete-active");
     }
-    function closeAllLists(elmnt) {
-        for (let x of document.getElementsByClassName("autocomplete-items")) {
-            if (elmnt != x && elmnt != element) { x.parentNode.removeChild(x) }
-        }
-    }
     document.addEventListener("click", function (e) {
-        closeAllLists(e.target)
+        closeAllLists(input, e.target)
     });
+}
+function closeAllLists(input, elmnt) {
+    for (let x of document.getElementsByClassName("autocomplete-items")) {
+        if (elmnt != x && elmnt != input) { x.parentNode.removeChild(x) }
+    }
 }
 function displayDeckFromURL() {
     const urlParams = new URLSearchParams(window.location.search)
     if (urlParams.has("twda_id")) {
-        getAndDisplayDeck(null, urlParams.get("twda_id"))
+        getDeckByID(null, urlParams.get("twda_id"))
     }
     else {
         clearSearch()
