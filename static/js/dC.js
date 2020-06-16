06af3e981333bf057031fc82e20eaacd3f2a4d2c
Spigushe <martin.cuchet@gmail.com>
2020-06-16 18:45:30 +0200

    fixed copy/paste error

--- static/js/dC.js
+++
@@ -1,4 +0,0 @@
-function dC(name) {
-    document.getElementById("card-image").src = '/static/img/card-images/'.concat(name, '.jpg');
-    document.getElementById("card-modal").style.display = "block";
-}
