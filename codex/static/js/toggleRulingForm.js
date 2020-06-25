c89372a0f505889c33b9e9f0acb567db47d8eb47
Spigushe <martin.cuchet@gmail.com>
2020-06-25 12:38:33 +0200

    Final push

---
+++ codex/static/js/toggleRulingForm.js
@@ -0,0 +1,8 @@
+function toggleRulingForm() {
+  let ruling = document.getElementById("ruling-form");
+  if (ruling.style.display == "none") {
+	ruling.style.display = "block";
+  } else {
+	ruling.style.display = "none";
+  }
+}
