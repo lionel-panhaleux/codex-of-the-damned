e5afe52657c057227620892f6dda2dde4f82de59
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2020-07-16 09:19:58 +0200

    Fix translations setup

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
 import setuptools
+import setuptools.command.develop
+import setuptools.command.install
 
 
-setuptools.setup()
+class InstallWithCompile(setuptools.command.install.install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        compiler.domain = ["messages"]
+        compiler.directory = "codex_of_the_damned/translations"
+        compiler.run()
+        super().run()
+
+
+class DevelopWithCompile(setuptools.command.develop.develop):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        compiler.domain = ["messages"]
+        compiler.directory = "codex_of_the_damned/translations"
+        compiler.run()
+        super().run()
+
+
+setuptools.setup(
+    cmdclass={"install": InstallWithCompile, "develop": DevelopWithCompile}
+)
