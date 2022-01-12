41c99d831da4551dbe46b4cb1694728e5fdc7952
Spigushe <martin.cuchet@gmail.com>
2022-01-12 23:33:04 +0100

    Add style to 'See more'
    - Add id and class to the p tag generated by JS
    - Fix `div#krcg-click-modal` width to center `p.krcg-modal-link` on bigger screens
    - Forced `display: none;` to `p.krcg-modal-link` on smaller screens

--- codex_of_the_damned/static/js/display.js
+++ codex_of_the_damned/static/js/display.js
@@ -16,12 +16,17 @@ function get_cardLink (card) {
 
 function extends_clickCard () {
     clean_modal()
+    // First create the link
     var link = document.createElement("a")
     link.href = get_cardLink(getName(this))
     link.target = "_blank"
     link.innerHTML = "See more about it"
+    // Put in in a p tag
     var see_more = document.createElement("p")
+    see_more.id = "krcg-modal-link"
+    see_more.className = "krcg-modal-link"
     see_more.append(link)
+    // Add the p tag after the image
     document.getElementById('krcg-click-image').after(see_more)
 }
 
