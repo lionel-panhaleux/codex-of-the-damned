a9fed3437778cf4ed9cea4bdbadc3604d84bfcca
Spigushe <martin.cuchet@gmail.com>
2022-01-17 22:11:42 +0100

    JS refactoring

--- codex_of_the_damned/static/js/display.js
+++ codex_of_the_damned/static/js/display.js
@@ -1,9 +1,3 @@
-function clean_modal() {
-    for (elem of document.querySelectorAll("#krcg-click-modal > p")) {
-        elem.remove()
-    }
-}
-
 function get_cardLink (card) {
     var origin = window.location.origin
     var pathname = window.location.pathname
@@ -14,22 +8,42 @@ function get_cardLink (card) {
     return pathname + card
 }
 
-function extends_clickCard () {
-    clean_modal()
-    // First create the link
-    var link = document.createElement("a")
-    link.href = get_cardLink(getName(this))
+function create_link () {
+    let link = document.createElement("a")
+    link.id = "krcg-tag-link"
     link.target = "_blank"
-    link.innerHTML = "See more about it"
-    // Put in in a p tag
-    var see_more = document.createElement("p")
+    let see_more = document.createElement("p")
     see_more.id = "krcg-modal-link"
-    see_more.className = "krcg-modal-link"
+    see_more.classList.add("krcg-modal-link")
     see_more.append(link)
-    // Add the p tag after the image
     document.getElementById('krcg-click-image').after(see_more)
 }
 
-for (elem of document.querySelectorAll(".krcg-card")) {
-    elem.addEventListener("click", extends_clickCard.bind(elem))
+function link_text () {
+    let lang = window.location.pathname.slice(1,3)
+    if (lang == "fr") return "En savoir plus"
+    if (lang == "es") return "Más información"
+    return "See more about it"
 }
+
+function serve_link (card) {
+    let link = document.getElementById("krcg-tag-link")
+    link.innerHTML = link_text()
+    link.href = get_cardLink(getName(card))
+}
+
+function extends_clickCard () {
+    if (document.getElementById("krcg-tag-link")) {
+        serve_link(this)
+    } else {
+        create_link()
+        serve_link(this)
+    }
+}
+
+window.addEventListener("load", (e) => {
+    // add new event listener on all page elements marked as cards
+    for (elem of document.querySelectorAll(".krcg-card")) {
+        elem.addEventListener("click", extends_clickCard.bind(elem))
+    }
+})
